f394ed4caf5a92c866d6252737168532a656b23e
mcraewhite <awhite2015@kellogg.northwestern.edu>
2015-03-16 16:53:56 -0500

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
   helper_method :current_user
-  #before_action :require_user
+  before_action :require_user
 
   def current_user
     User.find_by(id: session["user_id"])
